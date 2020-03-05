df68c6658a934bfa598918ba1119b7f44ea6b23e
Hammer Ádám <hammer.adam@thisisdone.com>
2020-03-05 16:57:01 +0100

    lots

--- assets/app.js
+++ assets/app.js
@@ -20,6 +20,11 @@ async function getMovieData(searchTerm = '', limit = 8, page = 1, order = 'downl
     return response.json();
 }
 
+async function getMovieDetails(id) {
+    const response = await fetch(`https://yts.mx/api/v2/movie_details.json?movie_id=${id}&with_images=true&with_cast=true`);
+    return response.json();
+}
+
 function appendMovieCards(data, container, clearContainer = false) {
     if (data.data.movie_count) {
         if (clearContainer) {
@@ -34,8 +39,10 @@ function appendMovieCards(data, container, clearContainer = false) {
 
                 // Create movie card
                 const card = document.createElement('div');
-                card.classList.add('movie-card');
-                
+                card.setAttribute('data-yts_id', movie.id);
+                card.classList.add('movie-card', 'z');
+                addCardClickListener(card);
+
                 // Add cover image
                 const img = document.createElement('img');
                 img.src = movie.medium_cover_image;
@@ -54,42 +61,97 @@ function appendMovieCards(data, container, clearContainer = false) {
                 title.href = `https://www.imdb.com/title/${movie.imdb_code}/`;
                 textWrapper.appendChild(title);
 
+                // Add download button
+                const button = document.createElement('button');
+                button.innerHTML = 'Download';
+                button.classList.add('btn', 'btn-primary', 'download-button');
+                addDownloadButtonClickListener(button);
+
+                // Add download link wrapper
+                const linkWrapper = document.createElement('div');
+                linkWrapper.classList.add('link-wrapper');
+                const linkWrapperClose = document.createElement('div');
+                linkWrapperClose.classList.add('link-wrapper__close');
+                linkWrapperClose.innerHTML = '&times';
+                addDownloadWrapperCloseClickListener(linkWrapperClose);
+                linkWrapper.appendChild(linkWrapperClose);
+
                 // Add download links
                 movie.torrents.forEach(torrent => {
                     const link = document.createElement('a');
                     link.innerHTML = `${torrent.quality} ${torrent.type}`;
                     link.href = torrent.url;
                     link.classList.add('download-link');
-                    textWrapper.appendChild(link);
+                    linkWrapper.appendChild(link);
+                    // textWrapper.appendChild(link);
                 });
 
                 // Add subtitle link
-                const subtitleLink = document.createElement('a');
-                subtitleLink.innerHTML = 'Subtitle';
-                subtitleLink.href = `https://www.yifysubtitles.com/movie-imdb/${movie.imdb_code}/`;
-                subtitleLink.target = '__blank';
-                subtitleLink.classList.add('subtitle-link');
-                textWrapper.appendChild(subtitleLink);
+                // const subtitleLink = document.createElement('a');
+                // subtitleLink.innerHTML = 'Subtitle';
+                // subtitleLink.href = `https://www.yifysubtitles.com/movie-imdb/${movie.imdb_code}/`;
+                // subtitleLink.target = '__blank';
+                // subtitleLink.classList.add('subtitle-link');
+                // textWrapper.appendChild(subtitleLink);
 
                 card.appendChild(textWrapper);
+                card.appendChild(button);
+                card.appendChild(linkWrapper);
                 // Append to DOM
                 wrapper.appendChild(card);
                 container.appendChild(wrapper);
+                matchHeight();
             });
         }
     }
 }
 
+function addCardClickListener(card) {
+    card.addEventListener('click', async function (e) {
+        if (
+            !e.target.classList.contains('download-link') &&
+            !e.target.classList.contains('title') &&
+            !e.target.classList.contains('download-button') &&
+            !e.target.classList.contains('link-wrapper__close')
+        ) {
+            const movieData = await getMovieDetails(card.getAttribute('data-yts_id'));
+            console.log(movieData);
+            const modal = document.getElementById('detail_modal');
+            document.querySelector('#detail_modal h3').innerHTML = movieData.data.movie.title;
+            modal.classList.add('show');
+            document.querySelector('.overlay').classList.add('show');
+            document.querySelector('body').classList.add('overflow-hidden');
+        }
+    });
+}
+
+function addDownloadButtonClickListener(button) {
+    button.addEventListener('click', function () {
+        this.parentNode.querySelector('.link-wrapper').classList.add('show');
+    });
+}
+
+function addDownloadWrapperCloseClickListener(button) {
+    button.addEventListener('click', function () {
+        this.parentNode.classList.remove('show');
+    });
+}
+
+document.querySelector('.detail_modal__close').addEventListener('click', function () {
+    document.getElementById('detail_modal').classList.remove('show');
+    document.querySelector('.overlay').classList.remove('show');
+    document.querySelector('body').classList.remove('overflow-hidden');
+});
+
 function matchHeight() {
     //Grab divs with the class name 'match-height'
-    var getDivs = document.getElementsByClassName('match-height');
+    const getDivs = document.getElementsByClassName('match-height');
 
     //Find out how my divs there are with the class 'match-height' 
-    var arrayLength = getDivs.length;
-    var heights = [];
+    const heights = [];
 
     //Create a loop that iterates through the getDivs variable and pushes the heights of the divs into an empty array
-    for (var i = 0; i < arrayLength; i++) {
+    for (let i = 0; i < getDivs.length; i++) {
         heights.push(getDivs[i].offsetHeight);
     }
 
@@ -99,10 +161,17 @@ function matchHeight() {
     }
 
     //Set a variable equal to the tallest div
-    var tallest = getHighest();
+    const tallest = getHighest();
 
     //Iterate through getDivs and set all their height style equal to the tallest variable
-    for (var i = 0; i < getDivs.length; i++) {
+    for (let i = 0; i < getDivs.length; i++) {
         getDivs[i].style.height = tallest + "px";
     }
 }
+
+document.addEventListener("DOMContentLoaded", async function() {
+    window.page = 1;
+    const data = await getMovieData('', 8, 1);
+
+    appendMovieCards(data, document.querySelector('.result-wrapper'), true);
+});
